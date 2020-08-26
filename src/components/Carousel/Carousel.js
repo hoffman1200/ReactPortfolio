811ebc0e3901dc66ac8680c96af2730efb632ebd
hoffman1200 <hoffman1200@hotmail.com>
2020-08-25 22:04:37 -0500

    Fixing Links

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -15,21 +15,21 @@ function Carousel() {
                     <img src="../Images/Laptop.jpg" className="d-block w-100" alt="..."/>
                     <div className="carousel-caption">
                         <h1 className="display-2">FSC Web Design</h1>
-                        <a href="/contact" target="_blank" className="btn btn-outline-light btn-lg" role="button" id="button1">Get in Touch!</a>
+                        <a href="/contact" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light btn-lg" role="button" id="button1">Get in Touch!</a>
                     </div>
                 </div>
                 <div className="carousel-item">
                     <img src="../Images/web.jpg" className="d-block w-100" alt="..."/>
                     <div className="carousel-caption">
                         <h1 className="display-2">Full Stack Developer</h1>
-                        <a href="/portfolio" target="_blank" className="btn btn-outline-light btn-lg" role="button" id="button2">Know my Work!</a>
+                        <a href="/portfolio" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light btn-lg" role="button" id="button2">Know my Work!</a>
                     </div>
                 </div>
                 <div className="carousel-item">
                     <img src="../Images/Code.jpg" className="d-block w-100" alt="..."/>
                     <div className="carousel-caption">
                         <h1 className="display-2" id="responsive">Responsive Web Design</h1>
-                        <a href="/portfolio" target="_blank" className="btn btn-outline-light btn-lg" role="button" id="button3">Check It Out!</a>
+                        <a href="/portfolio" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light btn-lg" role="button" id="button3">Check It Out!</a>
                     </div>
                 </div>
             </div>
